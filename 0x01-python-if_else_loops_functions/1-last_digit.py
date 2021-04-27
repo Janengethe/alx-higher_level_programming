c7f4796d9ffb8c1d38262c6e59da84fe338c56b0
Janengethe <janengethew@gmail.com>
2021-04-27 10:07:14 +0000

    reduced the number of characters in lines

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,11 @@ if number < 0:
 else:
     i = number % 10
 if i > 5:
-    print("Last digit of {:d} is {} and is greater than 5".format(number, i))
-elif i == 0:
-    print("Last digit of {:d} is {} and is 0".format(number, i))
+    print("Last digit of {:d} is {} and is greater than 5"
+          .format(number, i))
+elif i is 0:
+    print("Last digit of {:d} is {} and is 0"
+          .format(number, i))
 else:
-    print("Last digit of {:d} is {} and is less than 6 and not 0".format(number, i))
+    print("Last digit of {:d} is {} and is less than 6 and not 0"
+          .format(number, i))
