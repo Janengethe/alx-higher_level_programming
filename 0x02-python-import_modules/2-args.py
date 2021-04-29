894dff33f64e94dbef64c2bc261dc00e485dcfd9
Janengethe <janengethew@gmail.com>
2021-04-29 10:38:11 +0000

    initial commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    userinput = argv[1:]
+    number = len(userinput)
+    print("{:d} {:s}{:s}".format(number, "arguments" if (number) is not 1 else "argument", "." if (number) is 0 else ":"))
+    for index, arg in enumerate(userinput):
+        print("{:d}: {:s}".format(index + 1, arg))
