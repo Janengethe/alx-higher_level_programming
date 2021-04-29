df6dde9128119a485af254325ede70a60b6c58d0
Janengethe <janengethew@gmail.com>
2021-04-29 10:43:43 +0000

    checked the characters in line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ if __name__ == "__main__":
     from sys import argv
     userinput = argv[1:]
     number = len(userinput)
-    print("{:d} {:s}{:s}".format(number, "arguments" if (number) is not 1 else "argument", "." if (number) is 0 else ":"))
+    print("{:d} {:s}{:s}".format(number, "arguments"
+                                 if number is not 1 else "argument",
+                                 "." if number is 0 else ":"))
     for index, arg in enumerate(userinput):
         print("{:d}: {:s}".format(index + 1, arg))
