671c0d91d0d3efe851549cd7da5344e94a7b0145
Janengethe <janengethew@gmail.com>
2021-05-04 14:47:41 +0000

    changed for empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
-    else:
-        return (len(sentence), sentence[0])
+    length = len(sentence)
+    char = sentence[0] if length > 0 else None
+    return (length, char)
