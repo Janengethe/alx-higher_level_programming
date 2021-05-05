a9bd57a3fdc9cba112ad018f1fdba5fc4cef39be
Janengethe <janengethew@gmail.com>
2021-05-05 17:06:04 +0000

    initial commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    val = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in val:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, val[op](a,b)))
