1b655915f529ac3230cd5494cf07e4546b5b7994
Janengethe <janengethew@gmail.com>
2021-05-17 13:45:31 +0000

    increamented integer to print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    while i < x:
+    integer = 0
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            i += 1
-        except:
-            i += 1
+            integer += 1
+        except (TypeError, ValueError):
             continue
     print("")
-    return i
+    return integer
