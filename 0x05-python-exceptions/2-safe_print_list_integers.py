fb4363c04ccc64edec1164b5405b243e6cf3031d
Janengethe <janengethew@gmail.com>
2021-05-17 13:31:47 +0000

    function to print the first integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+        except:
+            i += 1
+            continue
+    print("")
+    return i
