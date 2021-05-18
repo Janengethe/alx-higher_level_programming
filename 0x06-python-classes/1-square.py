248063c4b85cdb64559f4051c282f74be9fd8e75
Janengethe <janengethew@gmail.com>
2021-05-18 14:23:59 +0000

    created private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 1-square"""
+
+
+class Square:
+    """created class"""
+    def __init__(self, __size):
+        """private instance attribute - size"""
+        self.__size = __size
