fbcc8a9b5ea9e58adbc4b135b619c33163956d63
Janengethe <janengethew@gmail.com>
2021-05-31 15:41:20 +0000

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Has a public instance method
+Raises an exception
+"""
+
+
+class BaseGeometry:
+    """With public instance method
+    methods:
+    area(self)
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
