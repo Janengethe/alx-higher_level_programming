fed6ba3eb0f81f62528404c1fcf0f35594b200e0
Janengethe <janengethew@gmail.com>
2021-05-31 19:46:22 +0000

    imy int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 100-my_int
+
+Class MyInt inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    Methods:
+        __init__(self, num)
+        __eq__(self, other)
+        __ne__(self, other)
+    """
+    def __init__(self, num):
+        """initialize num"""
+        self.num = num
+
+    def __eq__(self, other):
+        """Return True if both are equal"""
+        return self.num == other
+
+    def __ne__(self, other):
+        """Return True if both are not equal"""
+        return self.num != other
