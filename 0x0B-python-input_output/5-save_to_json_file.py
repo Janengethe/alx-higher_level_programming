761cd10be89a8896ee998dc5c90635c1510c250a
Janengethe <janengethew@gmail.com>
2021-06-01 19:12:22 +0000

    initial commit

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 7-save_to_json_file
+Contains function that writes Python obj to file using JSON represenation
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes Python obj to file using JSON represenation"""
+    import json
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
