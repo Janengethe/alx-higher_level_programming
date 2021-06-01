c0a8754ef6b5a4d9893a700b8e00020db43ac564
Janengethe <janengethew@gmail.com>
2021-06-01 19:08:05 +0000

    initial commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6-from_json_string
+Contains function that returns python data structure from JSON string
+"""
+
+
+def from_json_string(my_str):
+    """Returns python data structure from JSON string"""
+    import json
+
+    return json.loads(my_str)
