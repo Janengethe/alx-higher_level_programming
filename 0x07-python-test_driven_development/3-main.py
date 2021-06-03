0ddf94066fb672c982ea0397fe3e31850b9e17f4
Janengethe <janengethew@gmail.com>
2021-06-03 08:20:14 +0000

    test for 1 arg

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 
-say_my_name("John", "Smith")
+say_my_name("Jane", "Ng'ethe")
 say_my_name("Walter", "White")
 say_my_name("Bob")
 try:
