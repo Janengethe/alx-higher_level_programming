065b22d6c96a7c91a5e86811bb7a624c7b6859ef
Janengethe <janengethew@gmail.com>
2021-06-04 11:05:31 +0000

    args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,6 +14,7 @@ class Square(Rectangle):
     Methods:
         def __init__(self,size,x,y,id)
         def __str__(self)
+        def update(self, *args, **kwargs)
     Getter:
        def size(self)
     Setter:
@@ -44,3 +45,27 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {}/{} - {}".format
                 (self.id, self.x, self.y, self.size))
+
+    def update(self, *args, **kwargs):
+        """Assign arguments to attributes"""
+        if args:
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                elif i == 1:
+                    self.size = j
+                elif i == 2:
+                    self.x = j
+                else:
+                    self.y = j
+
+        else:
+            """kwargs"""
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
