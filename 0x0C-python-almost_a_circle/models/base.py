b10f3766fd41d6621396ba55f667098a785c8ebb
Janengethe <janengethew@gmail.com>
2021-06-04 13:42:26 +0000

    converting json string to python object

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,8 @@ Defined by private class attribute __nb_objects
 """
 
 
+import json
+
 class Base:
     """Class Base"""
     __nb_objects = 0
@@ -18,3 +20,33 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of
+        list dictionary
+        """
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of
+        list_objs to a file
+        list_objs is a list of instances who inherits of Base
+        """
+        objs = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            for ob in list_objs:
+                objs.append(cls.to_dictionary(ob))
+            with open(filename, "w") as f:
+                f.write(cls.to_json_string(objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return list of json string representation"""
+        if json_string is None or len(json_string) == 0:
+            json_string = "[]"
+        return json.loads(json_string)
