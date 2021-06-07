4ad8e04ec25b27b32aa5117e3717e11d19067ea8
Janengethe <janengethew@gmail.com>
2021-06-07 09:10:41 +0000

    csv

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,8 @@ Defined by private class attribute __nb_objects
 
 
 import json
+import csv
+
 
 class Base:
     """Class Base"""
@@ -66,4 +68,47 @@ class Base:
         """Returns a list of instances"""
         lists = []
         filename = cls.__name__ + ".json"
-        
+        try:
+            with open(filename, "r") as f:
+                instances = cls.from_json_string(f.read())
+            for k, v in enumerate(instances):
+                lists.append(cls.create(**instances[k]))
+
+        except:
+            pass
+        return lists
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes csv file"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline='') as f:
+            writer = csv.writer(f)
+            for ob in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([ob.id, ob.width, ob.height, ob.x, ob.y])
+                else:
+                    writer.writerow([ob.id, ob.size, ob.x, ob.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes csv file"""
+        ob = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, "r", newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    dic = {"id": int(row[0]),
+                           "width": int(row[1]),
+                           "height": int(row[2]),
+                           "x": int(row[3]),
+                           "y": int(row[4])}
+                if cls.__name__ == "Square":
+                    dic = {"id": int(row[0]),
+                           "size": int(row[1]),
+                           "x": int(row[2]),
+                           "y": int(row[3])}
+                objs = cls.create(**dic)
+                ob.append(objs)
+        return ob
