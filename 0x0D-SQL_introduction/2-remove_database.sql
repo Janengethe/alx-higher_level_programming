2d379d4170ca2f95be9f1482bb2d7a456b7d7b52
Janengethe <janengethew@gmail.com>
2021-07-12 13:36:45 +0000

    corrected a typo

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 --  a script that deletes the database hbtn_0c_0 in MySQL server
 -- the script does not fail if the database doesn't exist
-DROP DATABASE IF NOT EXIST hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
