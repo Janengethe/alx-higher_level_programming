df1fa4798669ed8428fd8655db579292380000ed
Janengethe <janengethew@gmail.com>
2021-07-13 15:36:44 +0000

    fixed typo

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,6 @@
 -- user granted only SELECT privilege
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON `hbtn_0d_2` TO 'user_0d_2'@'localhost';
