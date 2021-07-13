197d1d2fbe7f3b17e127927100d58d467372b601
Janengethe <janengethew@gmail.com>
2021-07-13 15:45:17 +0000

    changed permission to database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,7 @@
 -- creates database hbtn_0d_2 and user user_0d_2
 -- user granted only SELECT privilege
-
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE ON *. TO 'user_0d_2'@'localhost';
-GRANT SELECT ON `hbtn_0d_2` TO 'user_0d_2'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
