2717c13c6875490141c61f8358b4f46cdaa23dca
Janengethe <janengethew@gmail.com>
2021-08-06 10:58:15 +0000

    pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
-import MySQLdb
+"""
+Module 0-select_states
+a script that lists all states
+from the database hbtn_0e_0_usa
+"""
 
+
+import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=argv[1],
-                     password=argv[2],
-                     database=argv[3]
-)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-for data in cursor.fetchall():
-    print (data)
-db.close()
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    for data in cursor.fetchall():
+        print(data)
+    db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+"""
+Module 1-filter_states
+a script that lists all states with
+a name starting with N (upper N) from
+the database hbtn_0e_0_usa:
+"""
 
 import MySQLdb
-
 from sys import argv
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=argv[1],
-                     password=argv[2],
-                     database=argv[3]
-)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
-for data in cursor.fetchall():
-#    if data[1][0] == 'N':
-    print (data)
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE\
+    'N%' ORDER BY id ASC;")
+    for data in cursor.fetchall():
+        #    if data[1][0] == 'N':
+        print(data)
+    db.close()
