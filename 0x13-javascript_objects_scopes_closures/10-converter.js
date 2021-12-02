c11bfed32a9b6bab0eaf7e5edd2b974ae48919ab
Jane Ng'ethe <77954554+Janengethe@users.noreply.github.com>
2021-12-02 14:22:01 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-exports.converter = function (base) {
-  function conv (args) {
-    return args.toString(base);
-  }
-  return conv;
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-let num = -1;
-exports.logMe = function (item) {
-  num++;
-  console.log(num + ': ' + item);
-};
