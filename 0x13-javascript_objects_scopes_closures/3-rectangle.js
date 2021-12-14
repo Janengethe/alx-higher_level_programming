e03fafcba50fb8c576ef0d7eb32b5bd791b151f4
Janengethe <janengethew@gmail.com>
2021-12-14 01:22:17 -0800

    task 0-3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Instance method
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
