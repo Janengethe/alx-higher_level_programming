b3abcab853840a3ff10452f2416f96e89a618454
Janengethe <janengethew@gmail.com>
2022-01-09 01:19:08 -0800

    documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,7 @@ Fetches https://intranet.hbtn.io/status
 
 
 import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
