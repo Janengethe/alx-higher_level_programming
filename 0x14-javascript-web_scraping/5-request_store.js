9f258eb0b144f6b07f6d02acf368608d84f787dc
Janengethe <janengethew@gmail.com>
2022-02-01 05:23:37 -0800

    tasks

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
-let filename = process.argv[3];
+const url = process.argv[2];
+const filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
 
@@ -9,6 +9,6 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-      fs.writeFile(filename, 'utf8', body);
+    fs.writeFile(filename, 'utf8', body);
   }
 });
