5e54edfc40dd7db7ada5790a2ac8b0135e1b7755
Janengethe <janengethew@gmail.com>
2022-06-03 21:06:45 +0300

    group by genre_id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,6 +7,6 @@
 SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
+ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_shows DESC;
