d3ca51fd52ae22db0d07e8ffd0fbc92e028151e0
Janengethe <janengethew@gmail.com>
2022-06-03 17:12:33 +0300

    pascal's

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module 12-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns pascal's trangle(list of lists of integers)
+    or an empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
