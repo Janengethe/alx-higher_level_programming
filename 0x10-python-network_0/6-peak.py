2501c3041725679b48a157a455800e6f3e0aaf68
Janengethe <janengethew@gmail.com>
2022-06-03 14:41:29 +0300

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module 6-peak
+"""
+
+
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+    elif n == 2:
+        return max(list_of_integers)
+
+    mid = int(n / 2)
+    peak = list_of_integers[mid]
+
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
