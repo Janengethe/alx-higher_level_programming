a12bfca9b20dfca5b7b97d892a511fdbf3d2e69d
Janengethe <janengethew@gmail.com>
2022-06-03 17:06:58 +0300

    reload from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student():
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        if (type(attrs) == list and all(
+                type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
